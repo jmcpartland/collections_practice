b92d049415d8721f7718bacd0e0aedab336572aa
joemcpartland@gmail.com <josephmcpartland@JM.hsd1.co.comcast.net>
2021-02-10 13:05:40 -0700

    commit tree 8e824deebff696859e81bb045481974e0c42de8f

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(numbers)
+    numbers.sort
+    numbers.reverse
+end
 
+def sort_array_desc(numbers)
+    numbers.sort.reverse
+end
+
+def sort_array_char_count(strings)
+    strings.sort { |a,b| a.length <=> b.length }
+end
+
+def swap_elements(elements)
+    elements[1], elements[2] = elements[2], elements[1]
+    elements
+end
+
+def reverse_array(rev)
+    rev.reverse
+end
+
+def kesha_maker(money)
+    money.map.each {|a| a.gsub(a[2], "$")}
+end
+
+def find_a(strings)
+    strings.select { |a| a.start_with?("a")}
+end
+
+def sum_array(addition)
+    sum = 0
+    addition.each { |a| sum += a }
+    sum
+end
+
+def add_s(plural)
+    plural.collect do |word|
+      if plural[1] == word
+        word
+      else
+        word + "s"
+      end
+    end
+  end
